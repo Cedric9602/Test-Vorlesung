0dad376da46ea8e7ea31729ce03c432cc73fc3f4
Cedric9602 <cedric20@gmx.de>
2017-10-28 17:01:35 +0200

    Tests mit mehreren Klassen (funktioniert noch nicht)

--- 3_grafik/3_grafik.cpp
+++ 3_grafik/3_grafik.cpp
@@ -36,33 +36,39 @@ public:
 		, street("Bilder/street.png")
 		, player("Bilder/Panzer.png")
 	{
-		set_caption("Panzerspiel"); 
+		//set_caption("Panzerspiel"); 
 	}
-
+	
+	
 	struct Position_obj {
 		int16_t s = 3; //Spalte
 		int16_t x = 375; //eig. Koordinaten
 		int16_t y = 900;
 	};
-	
+
 	Position_obj Player;
-
-	struct enemy {
-
-	};
+	
 
 	bool input_LEFT_up = false;
 	bool input_RIGHT_up = false;
+	
 
+	/*TEST:
+	PLAYER player;
+
+	bool RIGHT_up = false;
+	*/
 
 	void update() override {
+		//TEST:
+		//player.r_step(player);
 		
 		if (input().down(Gosu::KB_LEFT) && !input_LEFT_up) {
 			input_LEFT_up = true;
 
 			if (Player.s > 1) {
 				Player.s = Player.s - 1;
-				Player.x = Player.x - 150; //Abfrage auf pos. Flanke
+				Player.x = Player.x - 150;
 			}
 		}
 		else if (!input().down(Gosu::KB_LEFT)) {
@@ -75,31 +81,25 @@ public:
 
 			if (Player.s < 5) {
 				Player.s = Player.s + 1;
-				Player.x = Player.x + 150; //Abfrage auf pos. Flanke
+				Player.x = Player.x + 150;
 			}
 		}
 		else if (!input().down(Gosu::KB_RIGHT)) {
 			input_RIGHT_up = false;
-		}
-
-		/*if (input().down(Gosu::KB_RIGHT)) {
-			if (Player.s < 5) {
-				Player.s = Player.s + 1;
-				Player.x = Player.x + 150;  //Abfrage auf pos. Flanke
-			}
-		}*/
-		
+		}		
 	}
 
 	void draw() override {
-		street.draw_rot(0, 0, 0.0,
-			0, // Rotationswinkel in Grad
-			0.0, 0.0 // Position der "Mitte" relativ zu x, y
+
+
+		street.draw_rot(0, 0, 0.1,
+			0,				// Rotationswinkel in Grad
+			0.0, 0.0		// Position der "Mitte" relativ zu x, y
 		);
 
-		player.draw_rot(Player.x, Player.y, 0.1,
-			0, // Rotationswinkel in Grad
-			0.5, 0.5 // Position der "Mitte" relativ zu x, y
+		player.draw_rot(Player.x, Player.y, 0.2,							// für test müsste das hier stehen: player.get_x(), player.get_y(), 0.2,
+			0,				// Rotationswinkel in Grad
+			0.5, 0.5		// Position der "Mitte" relativ zu x, y
 		);
 
 		/*
@@ -115,6 +115,65 @@ public:
 		*/
 	}
 };
+
+/*
+class INPUT : public GameWindow {
+	bool LEFT_up = false;
+	bool RIGHT_up = false;
+
+public:
+	void r_step(PLAYER player) {
+		if (input().down(Gosu::KB_RIGHT) && !RIGHT_up) {
+			RIGHT_up = true;
+
+			if (player.get_x() < 675) {
+				player.set_x(player.get_x() + 150);
+			}
+		}
+		else if (!input().down(Gosu::KB_RIGHT)) {
+			RIGHT_up = false;
+		}
+	}
+
+	void l_step(PLAYER player) {
+		if (input().down(Gosu::KB_LEFT) && !LEFT_up) {
+			LEFT_up = true;
+
+			if (player.get_x() > 75) {
+				player.set_x(player.get_x() - 150);
+			}
+		}
+		else if (!input().down(Gosu::KB_LEFT)) {
+			LEFT_up = false;
+		}
+	}
+};
+
+
+class PLAYER : public GameWindow, public INPUT {
+	int16_t x = 375;			// x-Koordinate
+	int16_t y = 900;			// y-Koordinate
+
+public:
+	void set_x(int16_t x) { this->x = x; }
+	int get_x() { return this->x; }
+
+	void set_y(int16_t y) {	this->y = y; }
+	int get_y() { return this->y; }
+};
+
+
+class ENEMY : public GameWindow {
+public:
+
+};
+
+
+class ELEMENT : public GameWindow {
+public:
+
+};
+*/
 
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
